b836c7f0bb8e3cf3773302defdf3517ad5533542
Paul Wolneykien <manowar@altlinux.org>
2017-11-10 19:10:55 +0300

    Implement journal reading (ISBC, draft)

--- src/lowlevel/ll_isbc.c
+++ src/lowlevel/ll_isbc.c
@@ -1,10 +1,15 @@
-#include "lowlevel.h"
 /*#include <opensc/opensc.h>*/
 #include "opensc.h"
+#include "isbc/pkcs11.h"
+#include <time.h>
+
+#include "lowlevel.h"
 
 struct context
 {
     sc_context_t *ctx;
+    CK_FUNCTION_LIST_PTR p11;
+    CK_SESSION_HANDLE session;
 };
 
 struct connected
@@ -31,12 +36,15 @@ open_context (void)
     int r = sc_context_create (&(context->ctx), &params);
 	if (r != SC_SUCCESS || !context->ctx)
     {
-	ERR1 ("Failed to create OpenSC context: %s", sc_strerror (r));
-        free (context);
-		return NULL;
+        ERR1 ("Failed to create OpenSC context: %s", sc_strerror (r));
+        goto err;
 	}
     
     return context;
+
+ err:
+    free (context);
+    return NULL;
 }
 
 static void
@@ -174,6 +182,120 @@ pin_count (void *_context, unsigned int slot_num, int sopin)
 }
 
 static void
+set_session (void *context, CK_SESSION_HANDLE session)
+{
+    if (!_context) return;
+    struct context *context = (struct context *) _context;
+    context->session = session;
+}
+
+#define EVENT_INITIAL          0x01
+#define EVENT_INIT_TOKEN       0x02
+#define EVENT_USER_PIN_CHANGED 0x03
+#define EVENT_SO_PIN_CHANGED   0x04
+#define EVENT_USER_PIN_ERROR   0x05
+#define EVENT_SO_PIN_ERROR     0x06
+
+#define EVENT_FLAG_UNKNOWN     0x40
+#define EVENT_FLAG_MINIDRIVE   0x80
+
+struct jorunal_record
+{
+    uint32_t timestamp;
+    uint8_t  event_type;
+    uint8_t  _reserved_;
+};
+
+static int
+pin_needs_change (void *_context, unsigned int slot_num, int sopin)
+{
+    if (!_context) return -1;
+    struct context *context = (struct context *) _context;
+
+    if (!context->p11)
+    {
+        ERR ("Needs PKCS#11 handle. Please, set \"p11\" before module initialization");
+        return -1;
+    }
+    if (!context->session)
+    {
+        ERR ("Needs PKCS#11 session. Please, use set_session() to set it");
+        return -1;
+    }
+
+    CK_ULONG len = 0L;
+	if (p11->C_ISBC_ScribbleRead (context->session, 0, NULL, &len) != CKR_OK) {
+		ERR ("Journal reading error");
+        return -1;
+	}
+
+	if (0 == len)
+	{
+		DBG ("Journal is empty");
+        return PIN_NOT_INITIALIZED;
+	}
+
+    jorunal_record *recs = calloc (len / sizeof (jorunal_record), sizeof (jorunal_record));
+    if (!recs)
+    {
+        ERR ("Unable to allocate memory for the journal!");
+        return -1;
+    }
+    
+	if (p11->C_ISBC_ScribbleRead (context->session, 0, recs, &len) != CKR_OK)
+	{
+		ERR ("Journal reading error (2)");
+        return -1;
+	}
+
+    int initizlized = 0;
+    time_t user_last_changed = (time_t) 0;
+    time_t so_last_changed = (time_t) 0;
+    
+    jorunal_record *rec = recs;
+	while (rec < (recs + len / sizeof (jorunal_record)))
+	{
+        switch (rec->event_type)
+        {
+        case EVENT_INITIAL:
+        case EVENT_INIT_TOKEN:
+            initizlized = 1;
+            break;
+        case EVENT_USER_PIN_CHANGED:
+        case EVENT_USER_PIN_CHANGED | EVENT_FLAG_MINIDRIVE:
+            // FIXME: host byte order
+            if ((time_t) rec->timestamp > user_last_changed)
+                user_last_changed = (time_t) rec->timestamp;
+            break;
+        case EVENT_SO_PIN_CHANGED:
+        case EVENT_SO_PIN_CHANGED | EVENT_FLAG_MINIDRIVE:
+            // FIXME: host byte order
+            if ((time_t) rec->timestamp > so_last_changed)
+                so_last_changed = (time_t) rec->timestamp;
+            break;
+        }
+        rec++;
+	}
+
+    free (recs);
+
+    if (!initialized)
+        return PIN_NOT_INITIALIZED;    
+
+    if (sopin && so_last_changed == (time_t) 0)
+        return PIN_DEFAULT;
+    if (user_last_changed == (time_t) 0)
+        return PIN_DEFAULT;
+    
+    time_t now = time ();
+    
+    if (sopin && (now - so_last_changed) > context->expiration_period)
+        return PIN_EXPIRED;
+    if (now - user_last_changed > context->expiration_period)
+        return PIN_EXPIRED;
+}
+
+static void
 deinit (void *_context)
 {
     if (!_context) return;
@@ -184,6 +306,7 @@ deinit (void *_context)
 lowlevel_module* lowlevel_module_init (lowlevel_module *module) {
     module->pin_count = pin_count;
     module->context = open_context ();
+    module->context.p11 = module->p11;
     module->deinit = deinit;
     return module;
 }
